d575c8e6a449f04250e36a1a630bb0d5ca213b97
vu_29 <vibhu.upamanyu@gmail.com>
2020-10-24 20:38:25 +0530

    quiz attempting back-end completed and tested

--- pariksha/student/routes.py
+++ pariksha/student/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template,Blueprint,redirect,url_for,request
+from flask import render_template,Blueprint,redirect,url_for,request,flash
 from flask_login import login_required,current_user,logout_user
+from pariksha import db
 from pariksha.student.utils import shuffle,random
+from pariksha.models import Student,Teacher,Quiz
 import copy
 
 
@@ -18,15 +20,26 @@ def home():
         return "unauthorized acess attempted you have been logged out"
     return render_template("student_home.html",quotes = quotes, title = "Home")
 
-@student.route("/quiz")
-def quiz():
-    
-    orig_questions = {
-    "Question 1 (ans is A)":["A","B","C","D"],
-    "Question 2 (ans is B)":["B","A","C","D"],
-    "Question 3 (ans is C)":["C","B","A","D"],
-    "Question 4 (ans is D)":["D","B","C","A"],
-    }
+@student.route("/quiz/<int:quiz_id>")
+@login_required
+def quiz(quiz_id):
+    if current_user.student is None:
+        logout_user()
+        return redirect(url_for('main.welcome'))
+    quiz = Quiz.query.filter_by(id = quiz_id).first_or_404()
+    if quiz in current_user.student.submitted_quiz:
+        flash('You have already submitted this quiz!!','warning')
+        return redirect(url_for('student.home'))
+    teacher_id_list = [teacher.id for teacher in current_user.student.taught_by.all()]
+    if quiz.teacher_id not in teacher_id_list:
+        flash("You are not allowed to access the page you requested","warning")
+        return redirect(url_for('student.home'))
+       
+    questions = quiz.questions
+    orig_questions = dict()
+    for question in questions:
+        orig_questions[str(question.question_desc)] = [str(question.option_1),str(question.option_2),str(question.option_3),str(question.option_4)]
+
 
     questions = copy.deepcopy(orig_questions)
     shuffled_q = shuffle(questions)
@@ -38,24 +51,45 @@ def quiz():
 
     return render_template('quiz.html', shuffled_questions = shuffled_q , questions = questions, title = "Quiz", question_count = question_count)
 
-@student.route("/quiz", methods = ["POST"])
-def quiz_post():
+@student.route("/quiz/<int:quiz_id>", methods = ["POST"])
+def quiz_post(quiz_id):
 
-    orig_questions = {
-    "Question 1 (ans is A)":["A","B","C","D"],
-    "Question 2 (ans is B)":["B","A","C","D"],
-    "Question 3 (ans is C)":["C","B","A","D"],
-    "Question 4 (ans is D)":["D","B","C","A"],
-    }
-
+    quiz = Quiz.query.filter_by(id = quiz_id).first_or_404()
+    if quiz in current_user.student.submitted_quiz:
+        flash('You have already submitted this quiz!!','warning')
+        return redirect(url_for('student.home'))
+    questions = quiz.questions
+    orig_questions = dict()
+    orig_questions_marks = dict()
+    for question in questions:
+        orig_questions[str(question.question_desc)] = [str(question.option_1),str(question.option_2),str(question.option_3),str(question.option_4)]
+        orig_questions_marks[str(question.question_desc)] = question.marks
+    
     questions = copy.deepcopy(orig_questions)
-    shuffled_q = shuffle(questions)
 
-    correct = 0
+    marks = 0
     for i in questions.keys():
         answered = request.form[i]
         if orig_questions[i][0] == answered:
-            correct = correct+1
-    return '<h1> Marks : '+str(correct) + '</h1>'
+            marks += orig_questions_marks[i]
+    
+    current_user.student.submitted_quiz.append(quiz)
+    db.session.commit()
+    db.session.execute(f'UPDATE submits_quiz SET marks = {marks} WHERE student_id = {current_user.student.id} and quiz_id = {quiz_id};')
+    db.session.commit()
+    flash(f'Your response for Quiz : {quiz.title} has been submitted','success')
+    return redirect(url_for('student.home'))
+
+@student.route("/list_quiz")
+@login_required
+def list_quiz():
+    if current_user.student is None:
+        logout_user()
+        return redirect(url_for('main.welcome'))
+    teachers_lis = current_user.student.taught_by.all()
+    quiz_lis = [quiz for teacher in teachers_lis for quiz in teacher.quiz_created]
+    return render_template('quiz_list.html',quiz_lis = quiz_lis)
+    
+    
 
 
