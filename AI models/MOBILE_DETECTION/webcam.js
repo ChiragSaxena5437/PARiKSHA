e7e2d581b942edd65cf3ad148b150b608ec563a9
Yoshi Bansal <Yoshibansal@users.noreply.github.com>
2020-10-31 10:43:55 +0530

    add mobile detection model

---
+++ AI models/MOBILE_DETECTION/webcam.js
@@ -0,0 +1,65 @@
+class Webcam {
+  constructor(webcamElement) {
+    this.webcamElement = webcamElement;
+  }
+
+  capture() {
+    return tf.tidy(() => {
+      // Reads the image as a Tensor from the webcam <video> element.
+      const webcamImage = tf.browser.fromPixels(this.webcamElement);
+
+      const reversedImage = webcamImage.reverse(1);
+
+      const croppedImage = this.cropImage(reversedImage);
+
+      // Expand the outer most dimension so we have a batch size of 1.
+      const batchedImage = croppedImage.expandDims(0);
+
+      return batchedImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
+    });
+  }
+
+  cropImage(img) {
+    const size = Math.min(img.shape[0], img.shape[1]);
+    const centerHeight = img.shape[0] / 2;
+    const beginHeight = centerHeight - (size / 2);
+    const centerWidth = img.shape[1] / 2;
+    const beginWidth = centerWidth - (size / 2);
+    return img.slice([beginHeight, beginWidth, 0], [size, size, 3]);
+  }
+
+  adjustVideoSize(width, height) {
+    const aspectRatio = width / height;
+    if (width >= height) {
+      this.webcamElement.width = aspectRatio * this.webcamElement.height;
+    } else if (width < height) {
+      this.webcamElement.height = this.webcamElement.width / aspectRatio;
+    }
+  }
+
+  async setup() {
+    return new Promise((resolve, reject) => {
+      navigator.getUserMedia = navigator.getUserMedia ||
+          navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
+          navigator.msGetUserMedia;
+      if (navigator.getUserMedia) {
+        navigator.getUserMedia(
+            {video: {width: 224, height: 224}},
+            stream => {
+              this.webcamElement.srcObject = stream;
+              this.webcamElement.addEventListener('loadeddata', async () => {
+                this.adjustVideoSize(
+                    this.webcamElement.videoWidth,
+                    this.webcamElement.videoHeight);
+                resolve();
+              }, false);
+            },
+            error => {
+              reject(error);
+            });
+      } else {
+        reject();
+      }
+    });
+  }
+}
